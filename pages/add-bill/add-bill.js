05df1ff275e946f4fea5bbf48c40e7e00f3c337a
louis li <louis.z.j.li@oocl.com>
2019-03-09 20:13:15 +0800

    update ui for add

--- pages/add-bill/add-bill.js
+++ pages/add-bill/add-bill.js
@@ -9,37 +9,105 @@ Page({
   data: {
     trd_session: "",
     billType: 0, //0: pay out;1:get in 
-    keyboardvalue: [
-      { value: 1 },
-      { value: 2 },
-      { value: 3 },
-      { value: 4 },
-      { value: 5 },
-      { value: 6 },
-      { value: 7 },
-      { value: 8 },
-      { value: 9 },
-      { value: "." },
-      { value: 0 },
-      { value: 'clear' },
+    keyboardvalue: [{
+        value: 1
+      },
+      {
+        value: 2
+      },
+      {
+        value: 3
+      },
+      {
+        value: 4
+      },
+      {
+        value: 5
+      },
+      {
+        value: 6
+      },
+      {
+        value: 7
+      },
+      {
+        value: 8
+      },
+      {
+        value: 9
+      },
+      {
+        value: "."
+      },
+      {
+        value: 0
+      },
+      {
+        value: 'clear'
+      },
     ],
 
-    keyboardvalue1: [
-      { value: '+' },
-      { value: '-' },
-      { value: '完成' }
+    keyboardvalue1: [{
+        value: '+'
+      },
+      {
+        value: '-'
+      },
+      {
+        value: '完成'
+      }
     ],
 
-    amount:0.00,
+    amount: 0.00,
     inputStr: [],
-    inputStrShow:''
+    inputStrShow: '',
+    date: '今天',
+    inputValue: '',
+    typeList: [{
+      imgUrl: '/images/food.png',
+      name: "餐饮"
+    }, {
+        imgUrl: '/images/shop.png',
+      name: "购物"
+    }, {
+        imgUrl: '/images/traffic.png',
+      name: "交通"
+      }, {
+        imgUrl: '/images/medical.png',
+        name: "医疗"
+      }, {
+        imgUrl: '/images/education.png',
+        name: "教育"
+      }, {
+        imgUrl: '/images/amusement.png',
+        name: "娱乐"
+      }, {
+        imgUrl: '/images/live.png',
+        name: "居住"
+      }, {
+        imgUrl: '/images/orther.png',
+        name: "其他"
+      },]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-   onLoad: function (options) {
-  
+  onLoad: function(options) {
+
+  },
+
+  bindKeyInput(e) {
+    console.log(e.detail.value)
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+
+  bindDateChange(e) {
+    this.setData({
+      date: e.detail.value
+    })
   },
 
   typeChange(e) {
@@ -67,22 +135,22 @@ Page({
       })
 
     }
-    if (value == 'clear' && this.data.inputStr.length!=0) {
+    if (value == 'clear' && this.data.inputStr.length != 0) {
       var popvalue = inputStr.pop();
 
       this.setData({
         inputStr: inputStr,
         amount: inputStr[inputStr.length - 1] == '+' ? parseInt(amount) - parseInt(popvalue) :
           (inputStr[inputStr.length - 1] == '-' ?
-            parseInt(amount) + parseInt(popvalue) : 
+            parseInt(amount) + parseInt(popvalue) :
             (inputStr.length == 0 ? 0 :
-             amount)),
+              amount)),
         inputStrShow: inputStr.join('')
       })
     }
-  
+
     if (value == '+' || value == '-') {
-      if (inputStr.length==0 || inputStr[inputStr.length - 1] == '+' || inputStr[inputStr.length - 1] == '-') {
+      if (inputStr.length == 0 || inputStr[inputStr.length - 1] == '+' || inputStr[inputStr.length - 1] == '-') {
         return;
       }
       inputStr.push(value);
@@ -95,7 +163,7 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
