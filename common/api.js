c7f56fb684e5f2f46abe2fa37099d903882cbfcb
AllenKuang <675383749@qq.com>
2019-03-09 22:04:58 +0800

    获取账单页面信息

--- common/api.js
+++ common/api.js
@@ -1,7 +1,11 @@
 
 //const globalUrl = "http://111.230.92.41:8000/"
-const globalUrl = "http://localhost:8888/api/v1"
+//const globalUrl = "http://liangca3-w10:8888/api/v1"
+const globalUrl = "http://tengte-w10:8888/api/v1"
 let requestUrl = {
   'login': globalUrl + '/login',//登录接口
+  'defaultMonthBill': globalUrl + '/accounts/month/',//获取月账单
+  'deleteRecord': globalUrl + '/accounts/',         //删除一条记录
+  'getOneRecordDetail': globalUrl + '/accounts/'    //获取一条记录的详情
 }
 export default requestUrl
